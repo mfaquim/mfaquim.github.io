4f2a59280dbecefe9f58df0f6067422c633c3d5c
cirobottini1 <112855183+cirobottini1@users.noreply.github.com>
2022-12-27 06:46:57 -0300

    mensagem

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+document.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 800) {
+    header.style.backgroundColor = "#3f4a57";
+    header.style.boxShadow =  "-1px 15px 10px #3f4a57";
+    header.style.transition = ".3s ease";
+  } else {
+    header.style.backgroundColor = "transparent";
+    header.style.boxShadow =  "none";
+  }
+});
+
 const hamburguer = document.querySelector(
   ".header .nav-bar .nav-list .hamburguer"
 );
@@ -12,14 +24,7 @@ hamburguer.addEventListener("click", () => {
   mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener("scroll", () => {
-  var scroll_position = window.scrollY;
-  if (scroll_position > 180) {
-    header.style.backgroundColor = "#29323c";
-  } else {
-    header.style.backgroundColor = "transparent";
-  }
-});
+
 
 menu_item.forEach((item) => {
   item.addEventListener("click", () => {
